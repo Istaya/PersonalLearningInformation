240b617f3fa6ac9db5600c3e1068c4c9e17f784f
OIllyasvielO <wang13312088162@gmail.com>
2022-07-08 14:18:17 +0800

    Update StudyLog01.cpp

--- ComputerGraphics/StudyLog01.cpp
+++ ComputerGraphics/StudyLog01.cpp
@@ -29,17 +29,17 @@
         #Dot Product(点乘)
         float k, xa, xb, xc, ya, yb, yc, za, zb, zc;
         Eigen::Vector3f A, B, C, Bp;
-        A · B == ||A|| * ||B|| * cosθ;
-        cosθ == A · B / (||A|| * ||B||) == A.normalized · B.normalized;
-        A · B == B · A;
-        A · (B + C) == (A · B) + (A · C);
-        (kA) · B == A · (kB) = k (A · B);
+        A.dot(B) == ||A|| * ||B|| * cosθ;
+        cosθ == A.dot(B) / (||A|| * ||B||) == A.normalized.dot(B.normalized);
+        A.dot(B) == B.dot(A);
+        A.dot(B + C) == A.dot(B) + A.cot(C);
+        (kA).dot(B) == A.dot(kB) = k * A.dot(B);
 
         #In 2D(二维计算)
-        A · B == xa * xb + ya * yb;
+        A.dot(B) == xa * xb + ya * yb;
 
         #In 3D(三维计算)
-        A · B == xa * xb + ya * yb + za * zb;
+        A.dot(B) == xa * xb + ya * yb + za * zb;
 
         #Projection(投影)
         //  B投影到A上
@@ -78,4 +78,36 @@
         X.cross(Y) == A * b;
 
         #Orthonormal
+        //Any set of 3 vectors (in 3D) that
+        Eigen::Vector3f U, V, W, P;
+        U.dot(V) == V.dot(W) == U.dot(W) == 0;
+        W == U.dot(V);
+        P == P.dot(U).cross(U) + P.dot(V).cross(V) + P.dot(W).cross(W);
+    }
+    Matrix(矩阵部分)
+    {
+        #Definition(定义)
+        //一个 m * n 的矩阵；
+        float m, n;
+        Eigen::MatrixXf N(m, n);
+        
+        #Multiplication(乘法)
+        float M1, M2, M3, M4, M5, M6;
+        float N1, N2, N3, N4, N5, N6 ,N7, N8;
+        Eigen::MatrixXf M(2, 3), N(4, 2), O;
+        M << M1, M2,
+             M3, M4,
+             M5, M6;
+        N << N1, N2, N3, N4,
+             N5, N6, N7, N8;
+        O << M1 * N1 + M2 * N5, M1 * N2 + M2 * N6, M1 * N3 + M2 * N7, M1 * N4 + M2 * N8,
+             M3 * N1 + M4 * N5, M3 * N2 + M4 * N6, M3 * N3 + M4 * N7, M3 * N4 + M4 * N8,
+             M5 * N1 + M6 * N5, M5 * N2 + M6 * N6, M5 * N3 + M6 * N7, M5 * N4 + M6 * N8;
+        O == M * N; 
+        Eigen::MatrixXf A, B, C;
+        A * B != B * A;
+        (A * B) * C = A * (B * C);
+        A * (B + C) = A * B + A * C;
+        (A + B) * C = A * C + B * C;
+
     }
